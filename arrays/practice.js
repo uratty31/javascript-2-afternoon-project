c1c5eb6d8ea5bc6b6a5bed62bccca5c816164006
uratty31 <U.31@live.com>
2020-09-03 16:06:05 -0600

    finished 1-5!

--- arrays/practice.js
+++ arrays/practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[2];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,10 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+for(let i = 0; i < family.length; i++){
+  alert(family[i]);}
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +70,10 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(let i = letters.length -1; i>=0; i--){
+    alert(letters[i])};
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +88,13 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(nums){
+  for(i=0; i<nums.length; i++){
+    if(nums[i] % 2 === 1){
+      nums.splice(i,1);}
+  }
+  return nums
+}
 
 
 
